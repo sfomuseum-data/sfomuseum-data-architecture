c1dd4b8465268d318710a239f5f835e67d6dbaa9
thisisaaronland <devnull@localhost>
2019-09-10 16:37:53 -0700

    start doing superseding

--- utils/python/phase-shift.py
+++ utils/python/phase-shift.py
@@ -5,10 +5,14 @@ import mapzen.whosonfirst.export
 
 import sys
 import os
+import copy
 import pprint
 
 if __name__ == "__main__":
 
+    sfo_id = 1159157271
+    inception = "2019-07-23"
+    
     arch = "/usr/local/data/sfomuseum-data-architecture"
     data = os.path.join(arch, "data")
 
@@ -28,16 +32,34 @@ if __name__ == "__main__":
 
     def update(wofid):
 
-        feature = mapzen.whosonfirst.utils.load(data, wofid)
-        props = feature["properties"]
-        wofid = props["wof:id"]
-        name = props["wof:name"]
-        
-        print "UPDATE %s (%s)" % (wofid, name)
+        old_feature = mapzen.whosonfirst.utils.load(data, wofid)
+        old_props = old_feature["properties"]
+        old_id = props["wof:id"]
+        old_name = props["wof:name"]
 
+        print "UPDATE %s (%s)" % (old_id, old_name)
+        
+        new_feature = copy.deepcopy(feature)
+        new_props = new_feature["properties"]
+
+        new_id = mapzen.whosonfirst.utils.generate_id()        
+        new_props["wof:id"] = new_id
+        new_props["edtf:inception"] = inception
+        new_props["wof:supersedes"] = [ wofid ]
+
+        new_feature["properties"] = new_props
+        exporter.export_feature(new_feature)        
+
+        old_props["edtf:cessation"] = inception
+        old_props["mz:is_current"] = 0
+        old_props["wof:superseded_by"] = [ new_id ]
+
+        old_feature["properties"] = old_props
+        exporter.export_feature(old_feature)
+        
         for child_id in rels.get(wofid, []):
             update(child_id)
         
-    update(1159157271)
+    update(sfo_id)
 
             
