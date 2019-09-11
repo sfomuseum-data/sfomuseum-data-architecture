cb46b1b0aa85d669614071e82a18eabb16bbfb1a
thisisaaronland <devnull@localhost>
2019-09-10 17:05:05 -0700

    wrangle parent stuff in phase shift tool

--- utils/python/phase-shift.py
+++ utils/python/phase-shift.py
@@ -16,6 +16,8 @@ if __name__ == "__main__":
     arch = "/usr/local/data/sfomuseum-data-architecture"
     data = os.path.join(arch, "data")
 
+    wof_data = "/usr/local/data/sfomuseum-data-whosonfirst/data"
+    
     crawl = mapzen.whosonfirst.utils.crawl(data, inflate=True)
     rels = {}
     
@@ -30,36 +32,55 @@ if __name__ == "__main__":
 
         rels[parentid] = children
 
-    def update(wofid):
+    def update(old_feature, parent):
 
-        old_feature = mapzen.whosonfirst.utils.load(data, wofid)
         old_props = old_feature["properties"]
-        old_id = props["wof:id"]
-        old_name = props["wof:name"]
+        old_id = old_props["wof:id"]
+        old_name = old_props["wof:name"]
+        old_placetype = props["sfomuseum:placetype"]
 
         print "UPDATE %s (%s)" % (old_id, old_name)
         
-        new_feature = copy.deepcopy(feature)
-        new_props = new_feature["properties"]
-
-        new_id = mapzen.whosonfirst.utils.generate_id()        
-        new_props["wof:id"] = new_id
-        new_props["edtf:inception"] = inception
-        new_props["wof:supersedes"] = [ wofid ]
-
-        new_feature["properties"] = new_props
-        exporter.export_feature(new_feature)        
-
         old_props["edtf:cessation"] = inception
         old_props["mz:is_current"] = 0
-        old_props["wof:superseded_by"] = [ new_id ]
+        
+        is_t1_gate = False
+
+        if old_placetype == "gate" and old_name.startswith("B"):
+            is_t1_gate = True
+
+        if not is_t1_gate:
+        
+            new_feature = copy.deepcopy(feature)
+            new_props = new_feature["properties"]
+        
+            new_id = mapzen.whosonfirst.utils.generate_id()        
+            new_props["wof:id"] = new_id
+
+            new_props["edtf:inception"] = inception
+            new_props["edtf:cessation"] = "open"
+            new_props["mz:is_current"] = 1
+            new_props["wof:supersedes"] = [ wofid ]
+
+            parent_props = parent["properties"]
+            new_props["wof:parent_id"] = parent_props["wof:id"]
+            new_props["wof:hierarchy"] = parent_props["wof:hierarchy"]
+            
+            new_feature["properties"] = new_props
+            # exporter.export_feature(new_feature)        
+
+            old_props["wof:superseded_by"] = [ new_id ]
 
         old_feature["properties"] = old_props
-        exporter.export_feature(old_feature)
+        # exporter.export_feature(old_feature)
         
-        for child_id in rels.get(wofid, []):
-            update(child_id)
-        
-    update(sfo_id)
+        for child_id in rels.get(old_id, []):
+            child_feature = mapzen.whosonfirst.utils.load(data, child_id)            
+            update(child_feature, new_feature)
+
+    sfo = mapzen.whosonfirst.utils.load(data, sfo_id)
+    parent = mapzen.whosonfirst.utils.load(wof_data, sfo["properties"]["wof:parent_id"])
+    
+    update(sfo, parent)
 
             
